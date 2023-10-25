ee88ee75a545a0f22711e8f4aaaa8d956a610cf0
Lithara Perera <litharaperera2002@gmail.com>
2023-10-25 20:20:09 +0530

    modified

--- nextapp/src/app/api/users/logout/route.ts
+++ nextapp/src/app/api/users/logout/route.ts
@@ -1,1 +1,12 @@
-// 2:09:01
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try{
+
+    } catch (error:any) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500}
+        );
+    }
+}
