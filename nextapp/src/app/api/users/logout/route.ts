98e0c641ec35933b47dec848904735558c4f6261
Lithara Perera <litharaperera2002@gmail.com>
2023-10-26 18:01:43 +0530

    modified

--- nextapp/src/app/api/users/logout/route.ts
+++ nextapp/src/app/api/users/logout/route.ts
@@ -2,10 +2,15 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try{
-        const response = NextResponse.json({
-            message: "Logout successfull",
-            success: true,
-        })
+        const response = NextResponse.json(
+            {
+                message: "Logout successfull",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+        return response;
+        
     } catch (error:any) {
         return NextResponse.json(
             { error: error.message },
