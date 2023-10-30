e5d4cf0bb129a165f87e8ab3c4a3df2cc8520632
Lithara Perera <litharaperera2002@gmail.com>
2023-10-31 02:51:00 +0530

    modified

--- nextapp/src/helpers/getDataFromToken.ts
+++ nextapp/src/helpers/getDataFromToken.ts
@@ -1,4 +1,14 @@
 import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
-// 2:30:30
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || '';
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!);
+        return decodedToken;
+
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
+}
+// 2:32:15
