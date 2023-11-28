56193ee7ab96655d39b3afe5d23e78f120a674ca
Lithara Perera <litharaperera2002@gmail.com>
2023-11-28 23:00:38 +0530

    modified

--- nextapp/src/app/api/users/me/route.ts
+++ nextapp/src/app/api/users/me/route.ts
@@ -10,9 +10,15 @@ connect();
 export async function GET(request:NextRequest){
     try {
         const userID = await getDataFromToken(request);
-        const user = await User.findOne({_id: userID})
+        const user = await User.findOne({_id: userID}).select("-password");
+        return NextResponse.json({
+            message: "User found",
+            data: user
+        });
 
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 400});
     }
 }
+
+// 2:37:48
