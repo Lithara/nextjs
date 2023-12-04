40aa35974eaf0d84a598950caa5fa006cbca8e53
Lithara Perera <litharaperera2002@gmail.com>
2023-12-04 18:46:25 +0530

    modified

--- nextapp/src/app/profile/page.tsx
+++ nextapp/src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
+import React, {useState} from "react";
 import Link from "next/link";
 import {toast} from "react-hot-toast";
 import {useRouter} from "next/navigation";
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [data, setData] = useState("nothinng")
     const logout = async () => {
         try {
             await axios.get('/api/users/logout')
@@ -19,11 +21,18 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2>{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}></Link>}</h2>
             <hr />
             <button 
             onClick={logout}
