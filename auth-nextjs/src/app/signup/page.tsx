cebb2aa6136155bbdb8d450e5a73fc55e960556b
Lithara Perera <litharaperera2002@gmail.com>
2023-12-15 00:38:44 +0530

    modified

--- auth-nextjs/src/app/signup/page.tsx
+++ auth-nextjs/src/app/signup/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+
 export default function SignupPage() {
-    return (
-        <div className="flex ">
-            <h1 className="text-center text-white text-2xl">Signup</h1>
-        </div>
-    )
+  return (
+    <div className="flex ">
+      <h1 className="text-center text-white text-2xl">Signup</h1>
+    </div>
+  );
 }
