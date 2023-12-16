a8955ac6660fe25eb4c34394c1b5d049a7624ede
Lithara Perera <litharaperera2002@gmail.com>
2023-12-16 13:08:48 +0530

    modified

--- auth-nextjs/src/app/signup/page.tsx
+++ auth-nextjs/src/app/signup/page.tsx
@@ -2,8 +2,14 @@
 import Link from "next/link";
 import React from "react";
 import {useRouter} from "next/navigation";
+import {axios} from "axios";
 
 export default function SignupPage() {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    });
   return (
     <div className="flex ">
       <h1 className="text-center text-white text-2xl">Signup</h1>
